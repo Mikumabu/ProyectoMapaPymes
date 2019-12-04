b9e01b546577babb53705dc8bb6b6f3e388a44b7
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2019-12-04 11:17:32 -0300

    Formulario

--- resources/views/Formulario/Formulario.blade.php
+++ resources/views/Formulario/Formulario.blade.php
@@ -4,8 +4,18 @@
     <input type="search"
            name="nombreEmpresa"
            class="form-control"
-           id="rut"
+           id="nombreEmpresa"
            placeholder="Ingrese Nombre Empresa"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Rut Empresa</p>
+    <input type="search"
+           name="rutEmpresa"
+           class="form-control"
+           id="rutEmpresa"
+           placeholder="Ingrese Rut Empresa"
     >
 </div>
 
@@ -14,7 +24,110 @@
     <input type="search"
            name="queOfrece"
            class="form-control"
-           id="rut"
+           id="queOfrece"
            placeholder="Categoría (Por ejemplo: Educación)"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Longitud en el Mapa</p>
+    <input type="search"
+           name="longitud"
+           class="form-control"
+           id="longitud"
+           placeholder="Longitud en el Mapa de Google"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Latitud en el Mapa</p>
+    <input type="search"
+           name="latitud"
+           class="form-control"
+           id="latitud"
+           placeholder="Latitud en el Mapa de Google"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Ubicación de Calle</p>
+    <input type="search"
+           name="calle"
+           class="form-control"
+           id="calle"
+           placeholder="Por Ejemplo: Blumel 1552"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Horario de Atención</p>
+    <input type="search"
+           name="horario"
+           class="form-control"
+           id="horario"
+           placeholder="Por ejemplo: 9:00 a 15:00"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Página de Facebook</p>
+    <input type="search"
+           name="facebook"
+           class="form-control"
+           id="facebook"
+           placeholder="Ingrese Link de Página Oficial"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">¿Formalizado?</p>
+    <select
+        id="formalizado"
+        name="formalizado"
+        class="form-control"
+        required>
+
+        <option value="Si">Si</option>
+        <option value="No">No</option>
+    </select>
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Comuna</p>
+    <input type="search"
+           name="comuna"
+           class="form-control"
+           id="comuna"
+           placeholder="Ingrese Comuna. Por Ejemplo: Antofagasta"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Contacto</p>
+    <input type="search"
+           name="contacto"
+           class="form-control"
+           id="contacto"
+           placeholder="Ingrese un Contacto. Por Ejemplo: Juan Pérez"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Teléfono</p>
+    <input type="search"
+           name="telefono"
+           class="form-control"
+           id="telefono"
+           placeholder="Ingrese un Teléfono"
+    >
+</div>
+
+<div class="col-md-4 mb-3">
+    <p align="left">Email</p>
+    <input type="search"
+           name="email"
+           class="form-control"
+           id="email"
+           placeholder="Ingrese un Email"
     >
 </div>
