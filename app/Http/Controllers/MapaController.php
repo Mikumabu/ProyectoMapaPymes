7cdce9cecdecdef310c5c2e68efa67906ef8e246
Manuel <manuelzuletab@gmail.com>
2019-12-06 13:14:44 -0300

    Geocoding: Cambio del orden de despliegue

--- app/Http/Controllers/MapaController.php
+++ app/Http/Controllers/MapaController.php
@@ -10,7 +10,6 @@ class MapaController extends Controller {
         $users = DB::table('formularios_aprobados')->get();
         foreach($users as $user){
             $nombreEmpresa = $user->nombre_empresa;
-            $rutEmpresa = $user->rut_empresa;
             $queOfrece = $user->categoria;
             $calle = $user->ubicacion;
             $horario = $user->horario;
@@ -24,11 +23,11 @@ class MapaController extends Controller {
             $descripcion = $user->descripcion;
             $latitud = $user->latitud;
             $longitud = $user->longitud;
-            Mapper::informationWindow($latitud, $longitud, '<b>Nombre Empresa: </b>'.$nombreEmpresa.'<br><b>RUT: </b>'.$rutEmpresa.
-                '<br><b>¿Qué ofrece? </b>'.$queOfrece.'<br><b>Dirección: </b>'.$calle.'<br><b>Horario atención: </b>'.$horario.
-                '<br><b>¿Formalizado? </b>'.$formalizado.'<br><b>Ciudad: </b>'.$comuna.'<br><b>Contacto: </b>'.$contacto.
-                '<br><b>Teléfono: </b>'.$telefono.'<br><b>Correo: </b>'.$email.'<br><b>Descripción: </b>'.$descripcion.
-                '<br><br><a href="https://www.facebook.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Facebook_icon_2013.svg/1024px-Facebook_icon_2013.svg.png"
+            Mapper::informationWindow($latitud, $longitud, '<b>Nombre Empresa: </b>'.$nombreEmpresa.'<br><b>Descripción: </b>'.$descripcion.
+                '<br><b>¿Qué ofrece? </b>'.$queOfrece.'<br><b>Dirección: </b>'.$calle.'<br><b>Ciudad: </b>'.$comuna.'
+                <br><b>Horario atención: </b>'.$horario.'<br><b>¿Formalizado? </b>'.$formalizado.'<br><b>Contacto: </b>'
+                .$contacto.'<br><b>Teléfono: </b>'.$telefono.'<br><b>Correo: </b>'.$email.'<br><br><a href="https://www.facebook.com/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Facebook_icon_2013.svg/1024px-Facebook_icon_2013.svg.png"
                 width="20" height="20"></a> <a href="https://www.instagram.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/600px-Instagram-Icon.png"
                 width="20" height="20"></a>', ['maxWidth' => 300, 'marker' => true/*, 'icon' => 'http://icons.iconarchive.com/icons/icons-land/points-of-interest/128/Golf-Club-Green-2-icon.png'*/]);
         }
