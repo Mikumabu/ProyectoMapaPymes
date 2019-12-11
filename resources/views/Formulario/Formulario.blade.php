2f6b3e64598ce06ac6cef4772a6c8f52f912276f
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2019-12-11 11:41:34 -0300

    Formulario Ordenado

--- resources/views/Formulario/Formulario.blade.php
+++ resources/views/Formulario/Formulario.blade.php
@@ -3,136 +3,142 @@
     {{ csrf_field() }}
 
 <div class="col-md-4 mb-3">
-    <p align="left">Nombre Empresa</p>
-    <input type="search"
-           name="nombreEmpresa"
-           class="form-control"
-           id="nombreEmpresa"
-           placeholder="Ingrese Nombre Empresa"
-    >
+    <p align="left">Nombre Empresa <input type="search"
+                                          name="nombreEmpresa"
+                                          class="form-control"
+                                          id="nombreEmpresa"
+                                          placeholder="Ingrese Nombre Empresa"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">Rut Empresa</p>
-    <input type="search"
-           name="rutEmpresa"
-           class="form-control"
-           id="rutEmpresa"
-           placeholder="Ingrese Rut Empresa"
-    >
+    <p align="left">Rut Empresa <input type="search"
+                                       name="rutEmpresa"
+                                       class="form-control"
+                                       id="rutEmpresa"
+                                       placeholder="Ingrese Rut Empresa"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">¿Qué Ofrece?</p>
-    <input type="search"
-           name="queOfrece"
-           class="form-control"
-           id="queOfrece"
-           placeholder="Categoría (Por ejemplo: Educación)"
-    >
+    <p align="left">Giro <input type="search"
+                                        name="queOfrece"
+                                        class="form-control"
+                                        id="queOfrece"
+                                        placeholder="Categoría (Por ejemplo: Educación)"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">Ubicación de Calle</p>
-    <input type="search"
-           name="calle"
-           class="form-control"
-           id="calle"
-           placeholder="Por Ejemplo: Blumel 1552"
-    >
+    <p align="left">Dirección de Calle <input type="search"
+                                              name="calle"
+                                              class="form-control"
+                                              id="calle"
+                                              placeholder="Por Ejemplo: Blumel 1552"
+        ></p>
+
+</div>
+
+
+<div class="col-md-4 mb-3">
+    <p align="left">Comuna <input type="search"
+                                  name="comuna"
+                                  class="form-control"
+                                  id="comuna"
+                                  placeholder="Ingrese Comuna. Por Ejemplo: Antofagasta"
+        ></p>
+
+</div>
+
+
+<div class="col-md-4 mb-3">
+    <p align="left">Horario de Atención <input type="search"
+                                               name="horario"
+                                               class="form-control"
+                                               id="horario"
+                                               placeholder="Por ejemplo: 9:00 a 15:00"
+        ></p>
+
+</div>
+
+
+
+<div class="col-md-4 mb-3">
+    <p align="left">¿Formalizado? <select
+            id="formalizado"
+            name="formalizado"
+            class="form-control"
+            required>
+
+            <option value="Si">Si</option>
+            <option value="No">No</option>
+        </select></p>
+
+</div>
+
+
+
+<div class="col-md-4 mb-3">
+    <p align="left">Representante <input type="search"
+                                    name="contacto"
+                                    class="form-control"
+                                    id="contacto"
+                                    placeholder="Ingrese un Contacto. Por Ejemplo: Juan Pérez"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">Horario de Atención</p>
-    <input type="search"
-           name="horario"
-           class="form-control"
-           id="horario"
-           placeholder="Por ejemplo: 9:00 a 15:00"
-    >
+    <p align="left">Teléfono <input type="search"
+                                    name="telefono"
+                                    class="form-control"
+                                    id="telefono"
+                                    placeholder="Ingrese un Teléfono"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">(Opcional) Página de Facebook</p>
-    <input type="search"
-           name="facebook"
-           class="form-control"
-           id="facebook"
-           placeholder="Ingrese Link de Facebook Oficial"
-    >
+    <p align="left">Email <input type="search"
+                                 name="email"
+                                 class="form-control"
+                                 id="email"
+                                 placeholder="Ingrese un Email"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">(Opcional) Página de Instagram</p>
-    <input type="search"
-           name="instagram"
-           class="form-control"
-           id="instagram"
-           placeholder="Ingrese Link de Instagram Oficial"
-    >
+    <p align="left">(Opcional) Página de Facebook <input type="search"
+                                                         name="facebook"
+                                                         class="form-control"
+                                                         id="facebook"
+                                                         placeholder="Ingrese Link de Facebook Oficial"
+        ></p>
+
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">¿Formalizado?</p>
-    <select
-        id="formalizado"
-        name="formalizado"
-        class="form-control"
-        required>
+    <p align="left">(Opcional) Página de Instagram <input type="search"
+                                                          name="instagram"
+                                                          class="form-control"
+                                                          id="instagram"
+                                                          placeholder="Ingrese Link de Instagram Oficial"
+        ></p>
 
-        <option value="Si">Si</option>
-        <option value="No">No</option>
-    </select>
 </div>
 
 <div class="col-md-4 mb-3">
-    <p align="left">Comuna</p>
-    <input type="search"
-           name="comuna"
-           class="form-control"
-           id="comuna"
-           placeholder="Ingrese Comuna. Por Ejemplo: Antofagasta"
-    >
-</div>
+    <p align="left">Descripcion <input type="search"
+                                       name="descripcion"
+                                       class="form-control"
+                                       id="descripcion"
+                                       placeholder="Descripción de la Empresa"
+        ></p>
 
-<div class="col-md-4 mb-3">
-    <p align="left">Contacto</p>
-    <input type="search"
-           name="contacto"
-           class="form-control"
-           id="contacto"
-           placeholder="Ingrese un Contacto. Por Ejemplo: Juan Pérez"
-    >
-</div>
-
-<div class="col-md-4 mb-3">
-    <p align="left">Teléfono</p>
-    <input type="search"
-           name="telefono"
-           class="form-control"
-           id="telefono"
-           placeholder="Ingrese un Teléfono"
-    >
-</div>
-
-<div class="col-md-4 mb-3">
-    <p align="left">Email</p>
-    <input type="search"
-           name="email"
-           class="form-control"
-           id="email"
-           placeholder="Ingrese un Email"
-    >
-</div>
-
-<div class="col-md-4 mb-3">
-    <p align="left">Descripcion</p>
-    <input type="search"
-           name="descripcion"
-           class="form-control"
-           id="descripcion"
-           placeholder="Descripción de la Empresa"
-    >
 </div>
 
 <div class="form-group mt-4">
