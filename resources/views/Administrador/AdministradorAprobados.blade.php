792f66def80f59fb718073527ebce5a408657742
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2019-12-12 13:52:56 -0300

    Ver Detalles avanzado

--- resources/views/Administrador/AdministradorAprobados.blade.php
+++ resources/views/Administrador/AdministradorAprobados.blade.php
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
@@ -47,8 +49,8 @@
                         </a>
                     </td>
                     <td>
-                        <a href="/Administrador/detallesAprobados/{{$formulario->id}}" class="btn btn-primary">
-                            <input type = "submit" value = "Ver Detalles">
+                        <a href="#" class="btn btn-primary pull-right" data-toggle="modal" data-target="#detalles" data-catid={{$formulario->id}}>
+                            Ver Detalles
                         </a>
                     </td>
                     <td>
@@ -65,6 +67,53 @@
 
     </div>
 </div>
+
+
+<div class="modal fade" id="detalles" style="width: 750px; margin: 100px auto; min-width: 1200px;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>×</span>
+                </button>
+                <h4>Detalles</h4>
+            </div>
+            <div class="modal-body" >
+
+                <table class="table table-striped table-sm">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Ubicación</th>
+                        <th scope="col">Horario</th>
+                        <th scope="col">Facebook</th>
+                        <th scope="col">Instagram</th>
+                        <th scope="col">Contacto</th>
+                        <th scope="col">Teléfono</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Descripción</th>
+
+                    </tr>
+                    </thead>
+                    @foreach($formularios_aprobados as $formulario)
+                        <tr>
+                            <td scope="row">{!! $formulario->ubicacion !!}</td>
+                            <td scope="row">{!! $formulario->horario !!}</td>
+                            <td scope="row">{!! $formulario->facebook !!}</td>
+                            <td scope="row">{!! $formulario->instagram !!}</td>
+                            <td scope="row">{!! $formulario->contacto !!}</td>
+                            <td scope="row">{!! $formulario->telefono !!}</td>
+                            <td scope="row">{!! $formulario->mail !!}</td>
+                            <td scope="row">{!! $formulario->descripcion !!}</td>
+
+                        </tr>
+                    @endforeach
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 @if ($message = Session::get('exito2'))
