0f2b0d6a9d9595e1afe00ce816c089a7189c0b73
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2019-12-12 11:12:08 -0300

    Avance de Vista

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -2,22 +2,25 @@
 <div class="nav">
     Mapa PYMES
     @if (Route::has('login'))
+        <a href="{{ url('/Mapa') }}">Home</a>
         @auth
-            <a class="dropdown-item" href="{{ route('logout') }}"
+            <a href="{{ url('/Administrador') }}">  Panel Administración</a>
+            <a href="{{ route('logout') }}"
                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
             </a>
+
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-            <a href="{{ url('/Administrador') }}">Panel Administración</a>
+
         @else
             <a href="{{ route('login') }}">Login</a>
             @if (Route::has('register'))
                 <a href="{{ route('register') }}">Register</a>
             @endif
         @endauth
-        <a href="{{ url('/Mapa') }}">Home</a>
+
     @endif
 </div>
