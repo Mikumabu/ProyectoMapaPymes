3940f364f8e60fa7a1b3d8404e05c9aff221ab8e
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2020-01-03 14:47:01 -0300

    Modal de Confirmar

--- resources/views/Administrador/AdministradorAprobados.blade.php
+++ resources/views/Administrador/AdministradorAprobados.blade.php
@@ -52,9 +52,39 @@
                             <a href="/Administrador/editarAprobado/{{$formulario->id}}" class="btn btn-primary">
                                 Detalles/Editar
                             </a>
-                            <button class="btn btn-danger" data-catid={{$formulario->id}} data-toggle="modal" data-target="#delete">Eliminar</button>
+                            <a class="btn btn-danger" href="#eliminar{{$formulario->id}}"data-toggle="modal">
+                                Eliminar
+                            </a>
                         </td>
                     </tr>
+
+                    <div class="modal modal-danger fade" id="eliminar{{$formulario->id}}" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title text-center" id="myModalLabel">Confirmación</h4>
+                                </div>
+
+                                <form action="{{route('administradorEliminar', ['id'=> $formulario->id])}}" method="post">
+                                    {{method_field('delete')}}
+                                    {{csrf_field()}}
+                                    <div class="modal-body">
+                                        <p class="text-center">
+                                            ¿Está seguro de eliminar esta solicitud?
+                                        </p>
+                                        <input type="hidden" name="category_id" id="cat_id" value="">
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+                                        <button type="submit" class="btn btn-warning">Sí</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
                 @endforeach
             </tbody>
         </table>
@@ -87,31 +117,4 @@
     });
 </script>
 
-<div class="modal modal-danger fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title text-center" id="myModalLabel">Confirmación</h4>
-            </div>
-
-                <form action="{{route('administradorEliminar',$formulario->id)}}" method="post">
-                    {{method_field('delete')}}
-                    {{csrf_field()}}
-                    <div class="modal-body">
-                        <p class="text-center">
-                            ¿Está seguro de eliminar esta solicitud?
-                        </p>
-                        <input type="hidden" name="category_id" id="cat_id" value="">
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
-                        <button type="submit" class="btn btn-warning">Sí</button>
-                    </div>
-                </form>
-
-        </div>
-    </div>
-</div>
 
