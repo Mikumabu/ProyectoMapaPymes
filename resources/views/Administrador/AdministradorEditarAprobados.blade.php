15d52a10ce392b11d5daf64c115c5f22c5180bb6
Juan C. Maury Rojas <jmr025@alumnos.ucn.cl>
2020-01-28 13:51:59 -0300

    Correcciones

--- resources/views/Administrador/AdministradorEditarAprobados.blade.php
+++ resources/views/Administrador/AdministradorEditarAprobados.blade.php
@@ -5,6 +5,25 @@
 
 <link rel="stylesheet" href="{{asset('/css/index.css')}}">
 @include('navbar.navbar')
+
+@if ($message = Session::get('exito1'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session::get('error1'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+<div>
+    <br>
+</div>
+
 <form method="POST" action="{{ route('actualizarFormularioAprobado') }}">
     {{ csrf_field() }}
     <div class="form-row justify-content-center">
@@ -205,19 +224,6 @@
     </div>
 </form>
 
-@if ($message = Session::get('exito1'))
-    <div class="alert alert-danger alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
-    </div>
-@endif
-
-@if ($message = Session::get('error1'))
-    <div class="alert alert-danger alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
-    </div>
-@endif
 
 <script>
     $(document).ready(function() {
